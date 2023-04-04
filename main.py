f14a896f3e358b4c5d0ae5e4f3b01b255c4a4607
Madhav Vij <madhav13vij@gmail.com>
2023-04-04 15:48:02 -0400

    formatting improvements
    Signed-off-by: Madhav Vij <madhav13vij@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import os
-import sys
+import datetime as dt
 import getpass
 import json
+import os
 import shutil
-import colorama
-from colorama import Fore, Back, Style
+import sys
 from urllib.parse import unquote, urlparse
-import datetime as dt
+
+import colorama
+from colorama import Back, Fore, Style
+
 colorama.init()
 
 
-def printWithColor(message: str, foreground_color: Fore=Fore.RESET, background_color: Back=Back.BLACK, end: str='\n'):
+def printWithColor(
+    message: str,
+    foreground_color: Fore = Fore.RESET,
+    background_color: Back = Back.BLACK,
+    end: str = "\n",
+):
     """Prints colored text if colorama is installed
 
     Args:
@@ -19,19 +26,21 @@ def printWithColor(message: str, foreground_color: Fore=Fore.RESET, background_c
         background_color (Back): Background coor of the text. Defaults to Back.BLACK.
         end (str, optional): end paramater of the print. Defaults to '\n'.
     """
-    
+
     print(foreground_color + background_color + message + Style.RESET_ALL, end=end)
 
 
 class Folder:
-    def __init__(self, path: str, workspace_exists: bool, is_old: bool, sizeinbytes: int) -> None:
+    def __init__(
+        self, path: str, workspace_exists: bool, is_old: bool, sizeinbytes: int
+    ) -> None:
         self.path: str = path
         self.workspace_exists: bool = workspace_exists
         self.is_old = is_old
         self.sizeinbytes: int = sizeinbytes
 
     def __repr__(self) -> str:
-        return f'(path: {self.path}, workspace_exists: {self.workspace_exists}, sizeinbytes: {self.sizeinbytes})'
+        return f"(path: {self.path}, workspace_exists: {self.workspace_exists}, sizeinbytes: {self.sizeinbytes})"
 
 
 def format_size(size_bytes: int) -> str:
@@ -45,13 +54,14 @@ def format_size(size_bytes: int) -> str:
     """
     if size_bytes < 1024:
         return f"{size_bytes} B"
-    elif size_bytes < 1024 ** 2:
+    elif size_bytes < 1024**2:
         return f"{size_bytes / 1024:.2f} KB"
-    elif size_bytes < 1024 ** 3:
+    elif size_bytes < 1024**3:
         return f"{size_bytes / (1024 ** 2):.2f} MB"
-    elif size_bytes < 1024 ** 4:
+    elif size_bytes < 1024**4:
         return f"{size_bytes / (1024 ** 3):.2f} GB"
     return f"{size_bytes / (1024 ** 4):.2f} TB"
+
 
 def getDefaultWSSFolderPath() -> str:
     """Returns default workspaceStorage path based on operating system that the script ran on
@@ -60,15 +70,18 @@ def getDefaultWSSFolderPath() -> str:
         str: Path of workspaceStorage folder
     """
     username = getpass.getuser()
-    
-    if sys.platform in ('linux', 'linux2'):
-        return f"/home/{username}/.config/Code/User/workspaceStorage/" 
-    elif sys.platform == 'darwin':
-        return f'/home/{username}/Library/Application Support/Code/User/workspaceStorage/'
-    elif sys.platform in ('win32', 'win64'):
-        return f'C:/Users/{username}/AppData/Roaming/Code/User/workspaceStorage/'
 
-    return ''
+    if sys.platform in ("linux", "linux2"):
+        return f"/home/{username}/.config/Code/User/workspaceStorage/"
+    elif sys.platform == "darwin":
+        return (
+            f"/home/{username}/Library/Application Support/Code/User/workspaceStorage/"
+        )
+    elif sys.platform in ("win32", "win64"):
+        return f"C:/Users/{username}/AppData/Roaming/Code/User/workspaceStorage/"
+
+    return ""
+
 
 def isValidWSSPath(path: str) -> bool:
     """Checks if the given folder path is a valid workspaceStorage folder
@@ -81,13 +94,14 @@ def isValidWSSPath(path: str) -> bool:
     """
     if not os.path.exists(path):
         return False
-    
+
     folders = os.listdir(path)
     for folder in folders:
         full_path = os.path.join(path, folder)
-        if not os.path.exists(os.path.join(full_path, 'workspace.json')):
+        if not os.path.exists(os.path.join(full_path, "workspace.json")):
             return False
     return True
+
 
 def askForValidWSSPath() -> str:
     """Continuously asks user for a valid workspaceStorage path
@@ -100,8 +114,13 @@ def askForValidWSSPath() -> str:
         if isValidWSSPath(path):
             return path
 
-def askYesNoQuestion(questionBody: str, yes_patterns: list[str]=['y', 'yes'], no_patterns: list[str]=['n', 'no']) -> bool:
-    """Asks user a yes/no question. 
+
+def askYesNoQuestion(
+    questionBody: str,
+    yes_patterns: list[str] = ["y", "yes"],
+    no_patterns: list[str] = ["n", "no"],
+) -> bool:
+    """Asks user a yes/no question.
 
     Args:
         questionBody (str): Body of the question
@@ -112,19 +131,20 @@ def askYesNoQuestion(questionBody: str, yes_patterns: list[str]=['y', 'yes'], no
         bool: Returns true if lowered input is in yes_patterns, false if lowered input is in no_patterns
     """
     while True:
-        print(questionBody, end='')
-        printWithColor(' (', Fore.MAGENTA, end='')
-        printWithColor('y', Fore.GREEN, end='')
-        printWithColor('/', Fore.MAGENTA, end='')
-        printWithColor('n', Fore.RED, end='')
-        printWithColor(')', Fore.MAGENTA, end='')
-        inp = input(': ')
+        print(questionBody, end="")
+        printWithColor(" (", Fore.MAGENTA, end="")
+        printWithColor("y", Fore.GREEN, end="")
+        printWithColor("/", Fore.MAGENTA, end="")
+        printWithColor("n", Fore.RED, end="")
+        printWithColor(")", Fore.MAGENTA, end="")
+        inp = input(": ")
         if inp.lower() in yes_patterns:
             return True
         elif inp.lower() in no_patterns:
             return False
-        
+
         printWithColor("Please provide a valid answer...", Fore.RED)
+
 
 def getSizeOfFolder(path: str) -> int:
     """Calculates a foldersize recursively
@@ -145,6 +165,7 @@ def getSizeOfFolder(path: str) -> int:
             total_size += getSizeOfFolder(full_path)
     return total_size
 
+
 def parseWSSFolder(path: str) -> list[Folder]:
     """Parses workspaceStorage folder
 
@@ -154,45 +175,48 @@ def parseWSSFolder(path: str) -> list[Folder]:
     Returns:
         list[Folder]: List of folders
     """
-    
+
     result_list: list[Folder] = []
     for folder in os.listdir(path):
         full_path = os.path.join(path, folder)
-        json_text = ''
-        
-        with open(os.path.join(full_path, "workspace.json"), 'r') as file:
+        json_text = ""
+
+        with open(os.path.join(full_path, "workspace.json"), "r") as file:
             json_text = file.read()
         data = json.loads(json_text)
-        
-        if 'folder' not in data.keys():
+
+        if "folder" not in data.keys():
             continue
-        
-        target_folder_name: str = data['folder']
+
+        target_folder_name: str = data["folder"]
         target_folder_name = unquote(urlparse(target_folder_name).path)
-        
+
         # Consider a folder "old" if it isn't modified in the last 30 days
         lastmodified = dt.datetime.fromtimestamp(os.path.getmtime(full_path))
         now = dt.datetime.now()
         delta = now - lastmodified
         is_old = delta.days > 30
-        
-        result_list.append(Folder(
-            full_path,
-            os.path.exists(target_folder_name),
-            is_old,
-            getSizeOfFolder(full_path),
-        ))
-    
+
+        result_list.append(
+            Folder(
+                full_path,
+                os.path.exists(target_folder_name),
+                is_old,
+                getSizeOfFolder(full_path),
+            )
+        )
+
     return result_list
+
 
 def main():
     printWithColor("Looking for workspaceFolder path...", Fore.BLUE)
     wss_path = getDefaultWSSFolderPath()
-    
+
     if not wss_path:
         printWithColor("This OS is not supported.", Fore.RED)
         return
-    
+
     if not isValidWSSPath(wss_path):
         printWithColor("Script couldn't find workspaceStorage folder.", Fore.YELLOW)
         askForValidWSSPath()
@@ -200,7 +224,7 @@ def main():
         printWithColor(f"Found workspaceStorage folder in {wss_path}", Fore.GREEN)
         if askYesNoQuestion("Do you want to provide an alternative path?"):
             askForValidWSSPath()
-        
+
     folders = parseWSSFolder(wss_path)
     # Mark old and/or unused workspaces as unwanted
     unwanted_folders = [x for x in folders if x.is_old or not x.workspace_exists]
@@ -208,50 +232,58 @@ def main():
     unwanted_size_formatted = format_size(unwanted_size)
     total_size = getSizeOfFolder(wss_path)
     total_size_formatted = format_size(total_size)
-    
+
     if len(unwanted_folders) == 0:
         printWithColor("No unwanted workspaceStorage folder found!", Fore.GREEN)
         return
-    
-    percentage = round(100 * unwanted_size/total_size, 2)
-    
-    printWithColor("Found ", end='')
-    printWithColor(str(len(unwanted_folders)), Fore.CYAN, end='')
-    printWithColor(f" folder{'s' if len(unwanted_folders) > 1 else ''} with total size of ", end='')
-    printWithColor(unwanted_size_formatted, Fore.CYAN, end='')
+
+    percentage = round(100 * unwanted_size / total_size, 2)
+
+    printWithColor("Found ", end="")
+    printWithColor(str(len(unwanted_folders)), Fore.CYAN, end="")
+    printWithColor(
+        f" folder{'s' if len(unwanted_folders) > 1 else ''} with total size of ", end=""
+    )
+    printWithColor(unwanted_size_formatted, Fore.CYAN, end="")
     printWithColor(f". ({Fore.CYAN}{percentage}%{Fore.RESET} of total)")
-    
-    if askYesNoQuestion(f"Do you want to clear {Fore.CYAN}ALL{Fore.RESET} unwanted folders?"):
-        printWithColor("Removing ", end='')
-        printWithColor(str(len(unwanted_folders)), Fore.CYAN, end='')
+
+    if askYesNoQuestion(
+        f"Do you want to clear {Fore.CYAN}ALL{Fore.RESET} unwanted folders?"
+    ):
+        printWithColor("Removing ", end="")
+        printWithColor(str(len(unwanted_folders)), Fore.CYAN, end="")
         printWithColor(f" folder{'s' if len(unwanted_folders) > 1 else ''}.")
         try:
             for (i, folder) in enumerate(unwanted_folders):
-                print(f'\rRemoving {os.path.basename(os.path.normpath(folder.path))} ', end='')
-                printWithColor('(', Fore.MAGENTA, end='')
-                printWithColor(str(i+1), Fore.CYAN, end='')
-                printWithColor('/', Fore.MAGENTA, end='')
-                printWithColor(str(len(unwanted_folders)), Fore.CYAN, end='')
-                printWithColor(')', Fore.MAGENTA, end='')
-                print(' ' * 10, end='')
-                
+                print(
+                    f"\rRemoving {os.path.basename(os.path.normpath(folder.path))} ",
+                    end="",
+                )
+                printWithColor("(", Fore.MAGENTA, end="")
+                printWithColor(str(i + 1), Fore.CYAN, end="")
+                printWithColor("/", Fore.MAGENTA, end="")
+                printWithColor(str(len(unwanted_folders)), Fore.CYAN, end="")
+                printWithColor(")", Fore.MAGENTA, end="")
+                print(" " * 10, end="")
+
                 shutil.rmtree(folder.path)
             print()
         except KeyboardInterrupt:
             printWithColor("Got KeyboardInterrupt. Aborting...", Fore.RED)
         except Exception as e:
-            printWithColor(f"Catched an exception while removing folders: {e}. Aborting...")
+            printWithColor(
+                f"Catched an exception while removing folders: {e}. Aborting..."
+            )
         else:
             printWithColor("Succesfully cleared all unused folders!", Fore.GREEN)
     else:
         printWithColor("Aborting...", Fore.RED)
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         printWithColor("\nGot KeyboardInterrupt. Aborting...", Fore.RED)
     except Exception as e:
         printWithColor(f"\nGot an exception while executing script: {e}", Fore.YELLOW)
-
